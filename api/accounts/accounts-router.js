352b33f0836ba2d51a906ee3890de76727467f9f
SylvaDev <irvingcsylva@gmail.com>
2025-09-13 20:18:14 -0700

    endpoints all wired and working

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,45 +1,40 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // Do your magic
   try {
-    
+    res.json([{}, {}])
   } catch (err) {
-    next(err)
+    next({ status: 422, message: 'error in the GET request to /' })
   }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
   try {
-
+    res.json('get accounts by id')
   } catch (err) {
     next(err)
   }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
   try {
-
+    res.json('post accounts')
   } catch (err) {
     next(err)
   }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
   try {
-
+    res.json('edit accounts by id')
   } catch (err) {
     next(err)
   }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
   try {
-
+    res.json('delete accounts by id')
   } catch (err) {
     next(err)
   }
